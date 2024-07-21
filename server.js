efe2ba5ca10318e848a8b6d2006de4eec661d06d
Sarthak Bhatt <sarthakbhatt2211@gmail.com>
2024-07-21 07:41:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,13 +8,16 @@ import cors from "cors"
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import path from 'path'
-
+import { fileURLToPath } from 'url'
 //config env
 dotenv.config();
 
 //databseconfig
 connectDB();
 
+//esmodeule fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 //rest object
 const app = express()
 
