6dac46d24f6dbb6d6abc60226be517bdaccf948e
Sarthak Bhatt <sarthakbhatt2211@gmail.com>
2024-09-11 18:17:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ dotenv.config();
 connectDB();
 
 //esmodeule fix
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 //rest object
 const app = express()
 
@@ -25,7 +25,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(morgan('dev'))
-app.use(express.static(path.join(__dirname, './client/build')))
+// app.use(express.static(path.join(__dirname, './client/build')))
 
 //routes
 app.use('/api/v1/auth', authRoutes);
